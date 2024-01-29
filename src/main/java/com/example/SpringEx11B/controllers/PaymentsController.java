3fac8bef8d39ad43d940a36ce4fa3e13a6c3e0ae
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2024-01-28 23:43:01 -0300

    # Fetch payment info with OpenFeign from another API

---
+++ src/main/java/com/example/SpringEx11B/controllers/PaymentsController.java
@@ -0,0 +1,25 @@
+package com.example.SpringEx11B.controllers;
+
+import com.example.SpringEx11B.models.Payment;
+import com.example.SpringEx11B.proxies.PaymentsProxy;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentsController {
+
+    private final PaymentsProxy paymentsProxy;
+
+    public PaymentsController(PaymentsProxy paymentsProxy) {
+        this.paymentsProxy = paymentsProxy;
+    }
+
+    @PostMapping("/payment")
+    public Payment createPayment(@RequestBody Payment payment) {
+        String requestId = UUID.randomUUID().toString();
+        return paymentsProxy.createPayment(requestId, payment);
+    }
+}
