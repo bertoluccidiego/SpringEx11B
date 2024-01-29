c2dc54564bd3ac2119aa7c3a7f151cb0c344cd67
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2024-01-28 23:26:03 -0300

    Initial commit

---
+++ src/main/java/com/example/SpringEx11B/SpringEx11BApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringEx11B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEx11BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEx11BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringEx11B/SpringEx11BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringEx11B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEx11BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
